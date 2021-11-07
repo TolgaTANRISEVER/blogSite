77686bcfb9f2e8dad1bf8748d1d8258387083435
Tolga <2017141006@cumhuriyet.edu.tr>
2021-11-07 15:46:27 +0300

    Migration Factory seed oluşturuldu seed yapısı bölündü

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,15 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        \App\Models\User::insert([
-            'name' => 'tolga',
-            'email' => 'tolga@gmail.com',
-            'email_verified_at' => now(),
-            'type'=>'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+            QuizSeeder::class,
+            UserSeeder::class,
         ]);
-         \App\Models\User::factory(10)->create();
+       
+         
     }
 }
